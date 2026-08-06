98c559a35a6c1fce1b6f5b848c91abbdfcc88215
cardclown <cardclown@users.noreply.github.com>
2026-08-06 16:58:28 +0800

    功能：恢复登录图片验证码校验

--- ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/domain/model/LoginBody.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/domain/model/LoginBody.java
@@ -35,4 +35,14 @@ public class LoginBody implements Serializable {
      */
     private String tenantId;
 
+    /**
+     * 验证码
+     */
+    private String code;
+
+    /**
+     * 唯一标识
+     */
+    private String uuid;
+
 }
